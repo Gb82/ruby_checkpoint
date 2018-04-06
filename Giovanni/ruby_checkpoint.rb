0e9ae7f8583eeb11aeeb3591c5b7e428e442306c
Gb82 <giovanni.bondioli@gmail.com>
2018-04-06 15:45:23 +1000

    not happy

--- Giovanni/ruby_checkpoint.rb
+++ Giovanni/ruby_checkpoint.rb
@@ -4,7 +4,10 @@
 # "Would you take this rose, `person`, in exchange for giving an old beggar woman shelter from the bitter cold?"
 # Type your solution directly below this line:
 
-
+def offer_rose(person)
+  print 'offer_rose'
+  person.gets = 
+end
 
 
 
@@ -26,6 +29,8 @@ town = {
 # - Add "Belle" to the `guests` array
 # Type your solution directly below this line:
 
+residents.delete("Belle")
+
 
 
 
@@ -43,6 +48,10 @@ friends = ["Chip Potts", "Cogsworth", "Lumière", "Mrs. Potts"]
 # "Belle is friends with Lumière"
 # "Belle is friends with Mrs. Potts"
 # Type your solution directly below this line:
+
+item.each do |friends|
+  puts "The current array item is: #{friends}"
+end
 
 
 
